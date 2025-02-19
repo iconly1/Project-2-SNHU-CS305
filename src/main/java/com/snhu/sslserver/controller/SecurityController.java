49da52fb9c878fc83b52ae46cb75af3bb694f41f
Ian Coxon <68827201+iconly1@users.noreply.github.com>
2025-02-19 16:46:28 -0500

    added suppression file to suppress unused Vulnerabilites

--- src/main/java/com/snhu/sslserver/controller/SecurityController.java
+++ src/main/java/com/snhu/sslserver/controller/SecurityController.java
@@ -1,4 +1,4 @@
-package com.snhu.sslserver;
+package com.snhu.sslserver.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
